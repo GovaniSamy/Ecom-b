387114eda49d33a2c2f0d60a8fa9df6e70bb681d
ibrahim-ismail1 <139874702+ibrahim-ismail1@users.noreply.github.com>
2025-11-18 04:40:19 +0200

    Product Review

---
+++ Ecom.BLL/Service/Abstraction/IProductReviewService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.BLL.Service.Abstraction
+{
+    public interface IProductReviewService
+    {
+        Task<ServiceResponse<IEnumerable<ProductReviewGetVM>>> GetAllAsync();
+
+        Task<ServiceResponse<ProductReviewGetVM>> GetByIdAsync(int id);
+        Task<ServiceResponse<IEnumerable<ProductReviewGetVM>>> GetByProductIdAsync(int productId);
+
+        Task<ServiceResponse<IEnumerable<ProductReviewGetVM>>> GetByUserIdAsync(string userId);
+        Task<ServiceResponse<bool>> CreateAsync(ProductReviewCreateVM vm);
+        Task<ServiceResponse<bool>> UpdateAsync(ProductReviewUpdateVM vm);
+        Task<ServiceResponse<bool>> ToggleDeleteAsync(int id, string userModified);
+    }
+}
